d7e5c76aab557d97e00fdd04a21f015cb5400f06
vladislav1133 <vladislav1133light@ukr.net>
2019-11-10 17:25:10 +0200

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application. test text
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
